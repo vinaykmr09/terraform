ea039a83b6452815534aab2f7e5cd1a28e476946
vinay kumar <vinaykmr09@gmail.com>
2025-02-19 18:22:26 +0530

    Initial Terraform setup

---
+++ hello-world/main.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_providers {
+        docker = {
+            source ="kreuzwerker/docker"
+            version = "3.0.2"
+        }
+    }
+}
+provider "docker" {
+    host = "npipe:////./pipe/docker_engine"
+}
+resource "docker_image" "web-image" {
+    name= "nginx:latest"
+}
+resource "docker_container" "kumar" {
+    image = docker_image.web-image.name
+    name = "vinay"
+    ports {
+    internal = 80
+    external = 8080
+  }
+
+
+}
